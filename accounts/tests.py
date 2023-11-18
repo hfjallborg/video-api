a779eb5f6b14c33831b14522fa146292bc0a0a11
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2023-11-18 21:13:07 +0100

    Automatic token creation for user creation endpoint

--- accounts/tests.py
+++ accounts/tests.py
@@ -36,3 +36,13 @@ class UserViewsTesting(APITestCase):
         response = self.client.get(reverse('accounts:user_list'),
                                    HTTP_AUTHORIZATION=f'Token {self.staff_user_token}')
         self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_user_create(self):
+        response = self.client.post(reverse('accounts:user_list'),
+                                    {'username': 'new-user',
+                                     'password': 'newpassword123'},
+                                    HTTP_AUTHORIZATION=f'Token {self.staff_user_token}')
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        # Check if token has been generated
+        # .filter is used instead of get to avoid having to use a try/except to catch DoesNotExist
+        self.assertTrue(Token.objects.filter(user__username='new-user').exists())
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth import get_user_model
 
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from .serializers import UserSerializer
 
@@ -16,3 +19,14 @@ class UserListView(ListCreateAPIView):
     user_model = get_user_model()
     queryset = user_model.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user_model = get_user_model()
+            user = user_model.objects.create_user(**serializer.validated_data)
+            # Generate a token for the new user
+            Token.objects.create(user=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
