0d01143febb0a129be54dbc6a51cc085e2f59ef2
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2023-11-18 16:32:18 +0100

    Changed URL endpoint for videos (api/v/ -> api/videos/)

--- videos/urls.py
+++ videos/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'videos'
 
 urlpatterns = [
-    path('api/v/', views.VideoListView.as_view(), name='video_list'),
-    path('api/v/<str:public_id>/', views.VideoRetrieveView.as_view(), name='video_detail'),
-    path('api/v/<str:public_id>/source/', views.video_source_view, name='video_source'),
-  #  path('api/v/upload/', views.VideoUploadView.as_view(), name='video_upload')
+    path('api/videos/', views.VideoListView.as_view(), name='video_list'),
+    path('api/videos/<str:public_id>/', views.VideoRetrieveView.as_view(), name='video_detail'),
+    path('api/videos/<str:public_id>/source/', views.video_source_view, name='video_source'),
 ]
