65abd64e6c28e70ff6044ef672fd8824967e19d0
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2023-11-20 15:06:39 +0100

    Added DRF view for obtaining auth token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('api/users/', views.UserListView.as_view(), name='user_list'),
+
+    # Authentication views
+    path('api/auth/get_token/', obtain_auth_token, name='get_auth_token'),
 ]
